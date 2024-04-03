b275bbd10e2b96d35c0b214f6e670763cff83393
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-04-03 12:43:33 +0200

    Fetched all auctions in app.jsx. :)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import '../styles/App.css'
-import SingleAuction from './SingleAuction/SingleAuction'
+import SingleAuction from './SingleAuction'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [allAuctions, setAllAuctions] = useState(null)
+
+
+  //Gets all the Current auctions, they are stored in allAuctions. Send this as props for you components that needs the auctions.
+  useEffect(() => {
+    fetch('https://auctioneer.azurewebsites.net/auction/5mlk')
+      .then((response) => response.json())
+      .then((data) => setAllAuctions(data))
+      return () => {
+        setAllAuctions(null)      
+      }
+    }, [])
+
+    console.log(allAuctions)
 
   return (
     <>
