f4e3c4d4b96b9e5c03caa6a12fb5fe15abc8e4e9
Armin Torkzadeh <armin.torkzadeh@outllook.com>
2024-04-05 22:25:02 +0200

    create basic component done

--- src/auctionsite.jsx
+++ src/auctionsite.jsx
@@ -1,2 +1,10 @@
 // Lägger till React för att kunna använda React-komponenter i den här filen
 import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Välkommen till Home sidan! 🏠</h1>
+        </div>
+    );
+}
