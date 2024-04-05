90ee69b810ac45960cf704f65e3c9ba758938cbd
Armin Torkzadeh <armin.torkzadeh@outllook.com>
2024-04-05 23:14:49 +0200

    comment const home

--- src/auctionsite.jsx
+++ src/auctionsite.jsx
@@ -1,6 +1,8 @@
 // Lägger till React för att kunna använda React-komponenter i den här filen
 import React from "react";
 
+// Vi börjar med att lägga till en bit kod som gör en sida med en enkel React-komponent
+// Det här är starten. Vi säger att vi vill göra en sida som vi kallar för 'Home'.
 const Home = () => {
     return (
         <div>
