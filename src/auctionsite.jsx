e63f1514920d4ce81f9fbeb50ed2bb17bdb4d9fe
Armin Torkzadeh <armin.torkzadeh@outllook.com>
2024-04-05 23:42:04 +0200

    comment return

--- src/auctionsite.jsx
+++ src/auctionsite.jsx
@@ -4,6 +4,8 @@ import React from "react";
 // Vi börjar med att lägga till en bit kod som gör en sida med en enkel React-komponent
 // Det här är starten. Vi säger att vi vill göra en sida som vi kallar för 'Home'.
 const Home = () => {
+    // Vad sidan ska visa bestämmer vi här på sidan "Home".
+
     return (
         <div>
             <h1>Välkommen till Home sidan! 🏠</h1>
