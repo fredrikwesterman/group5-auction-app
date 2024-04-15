4dbcd3f5beea105fa38c9187d5bb0a68f4ade3c2
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-04-15 14:23:08 +0200

    button bugg fixed! yippie!! :D

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,8 +64,8 @@ const Home = () => {
                     <p><b>Start Price: </b>{auction.StartingPrice}</p>
                     <p><b>Created: </b>{auction.StartDate}</p>
                     <p><b>Ending: </b>{auction.EndDate}</p>
-                    <button className="btn btn-primary">
-                      <NavLink to={`/single-auction/${auction.AuctionID}`} state={{ auction }}>
+                    <button>
+                      <NavLink className="btn btn-primary" to={`/single-auction/${auction.AuctionID}`} state={{ auction }}>
                         Go to auction
                       </NavLink>
                     </button>
