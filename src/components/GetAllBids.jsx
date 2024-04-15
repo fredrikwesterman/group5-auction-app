ff2fa75672e7b59593f237c075261ff4e76a53a5
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-04-15 15:55:21 +0200

    formatting, done

--- src/components/GetAllBids.jsx
+++ src/components/GetAllBids.jsx
@@ -1,28 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 const GetAllBids = ({ auctionId, allBids, setAllBids }) => {
-    useEffect(() => {
-            fetch(`https://auctioneer2.azurewebsites.net/bid/5mlk/${auctionId}`)
-                .then((response) => response.json())
-                .then((data) =>  setAllBids(data))
-                .catch((error) => console.error('Error fetching auctions:', error));
-            }, [])
-            //allBids i dependencyarrayen gör så att vi får en infinite loop.
-            //vi sätter allBids i fetchen till data, och varje gång den sätter det
-            //kommer den rerenderara komponenten och köra om fetchen forever.
+  useEffect(() => {
+    fetch(`https://auctioneer2.azurewebsites.net/bid/5mlk/${auctionId}`)
+      .then((response) => response.json())
+      .then((data) => setAllBids(data))
+      .catch((error) => console.error("Error fetching auctions:", error));
+  }, []);
+  //allBids i dependencyarrayen gör så att vi får en infinite loop.
+  //vi sätter allBids i fetchen till data, och varje gång den sätter det
+  //kommer den rerenderara komponenten och köra om fetchen forever.
 
-    return (
-        <div>
-            <ul className='flex gap-4'>
-                {allBids && allBids.map(bid => (
-                    <li key={bid.BidID}>
-                        <p>Bid: {bid.Amount}kr</p>
-                        <p>Bidder: {bid.Bidder}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul className="flex gap-4">
+        {allBids &&
+          allBids.map((bid) => (
+            <li key={bid.BidID}>
+              <p>Bid: {bid.Amount}kr</p>
+              <p>Bidder: {bid.Bidder}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-export default GetAllBids
+export default GetAllBids;
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,30 +1,48 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const SearchResults = ({ searchAuctions }) => {
-    return (
-        <>
-        <div className='flex gap-4'>
-            {searchAuctions.map(auction => (
-                <div className="card w-200 bg-base-100 shadow-xl ">
-                <div key={auction.AuctionID} className="card-body">
-                    <h3>{auction.Title}</h3>
-                    <p><b>Seller:</b> {auction.CreatedBy}</p>
-                    <p><b>Description: </b>{auction.Description.slice(0, 20) + "..."}</p>
-                    <p><b>Start Price: </b>{auction.StartingPrice}</p>
-                    <p><b>Created: </b>{auction.StartDate}</p>
-                    <p><b>Ending: </b>{auction.EndDate}</p>
-                    <button>
-                        <NavLink className="btn btn-primary"  to={`/single-auction/${auction.AuctionID}`} state={{ auction }}>
-                            Go to auction
-                        </NavLink>
-                    </button>
-                </div>
-                </div>
-            ))}
-        </div>
-        <div className='divider'></div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex gap-4">
+        {searchAuctions.map((auction) => (
+          <div className="card w-200 bg-base-100 shadow-xl ">
+            <div key={auction.AuctionID} className="card-body">
+              <h3>{auction.Title}</h3>
+              <p>
+                <b>Seller:</b> {auction.CreatedBy}
+              </p>
+              <p>
+                <b>Description: </b>
+                {auction.Description.slice(0, 20) + "..."}
+              </p>
+              <p>
+                <b>Start Price: </b>
+                {auction.StartingPrice}
+              </p>
+              <p>
+                <b>Created: </b>
+                {auction.StartDate}
+              </p>
+              <p>
+                <b>Ending: </b>
+                {auction.EndDate}
+              </p>
+              <button>
+                <NavLink
+                  className="btn btn-primary"
+                  to={`/single-auction/${auction.AuctionID}`}
+                  state={{ auction }}
+                >
+                  Go to auction
+                </NavLink>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="divider"></div>
+    </>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
